356522027d8ae806fb76ba5d25846f0f25733136
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2020-10-07 22:05:11 -0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+""" Package for using OpenXNcrypt, a hash function that's 8192-bit. 
+    encrypt:
+       Encrypt/hash data. (Takes a long time)
+"""
+
+from encrypt import encrypt, test
+
+if __name__ == '__main__':
+    test()
